fd3f5b38c60fd25dfa7e4ac24c1a6ca0b8855a1a
TaiTai3006 <101327980+TaiTai3006@users.noreply.github.com>
2023-08-30 09:12:06 +0700

    [ADD] search by voice

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,64 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./_header.scss";
 
 import { FaBars } from "react-icons/fa";
 import { AiOutlineSearch } from "react-icons/ai";
 import { MdNotifications, MdApps } from "react-icons/md";
+import { BsMic, BsMicFill } from "react-icons/bs";
 import { useSelector } from "react-redux";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 const Header = ({ handleToggleSidebar }) => {
   const [input, setInput] = useState('')
+  const [typeMic, setTypeMic] = useState(false)
+
+  // const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  const {
+    transcript,
+    listening,
+  } = useSpeechRecognition();
+
   const navigate = useNavigate()
+
   const {user} = useSelector(state=> state.auth)
+
+  // const handleStartListening = () => {
+  //   const oscillator = audioContext.createOscillator();
+  //   oscillator.type = 'triangle';
+  //   oscillator.frequency.setValueAtTime(440, audioContext.currentTime);
+  //   oscillator.connect(audioContext.destination);
+  //   oscillator.start();
+  //   oscillator.stop(audioContext.currentTime + 0.5);
+  // };
+
   const handleSubmit = (e)=>{
     e.preventDefault()
     navigate(`/search/${input}`)
   }
+  const handleTypeMic = (e)=>{
+    e.preventDefault()
+    setTypeMic(!typeMic)
+    if(!typeMic){
+      // handleStartListening()
+      SpeechRecognition.startListening() 
+     
+    }else{
+      SpeechRecognition.stopListening()
+    }
+  }
+  useEffect(()=>{
+    setInput(transcript)
+  },[transcript])
+
+  useEffect(()=>{
+    if(!listening){
+      setTypeMic(false)
+    }
+  },[listening])
+
   return (
     <div className="border border-dark header">
+      {console.log(listening)}
       <FaBars className="header_menu" size={26} onClick={handleToggleSidebar} />
       <img onClick={()=>{navigate('/')}}
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/2560px-YouTube_full-color_icon_%282017%29.svg.png"
@@ -24,19 +67,22 @@ const Header = ({ handleToggleSidebar }) => {
       />
       <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Search" value={input} onChange={e=> setInput(e.target.value)}/>
-        <button type="submit">
+        <button type="submit" style={{padding: 0}}>
           <AiOutlineSearch size={22} />
+        </button>
+        <button onClick={handleTypeMic}>
+          {!typeMic ? <BsMic size={22} /> :  <BsMicFill size={22}/>}
         </button>
       </form>
 
-      <dic className="header_icons">
+      <div className="header_icons">
         <MdNotifications size={28} />
         <MdApps size={28} />
         <img
           src={user?.photoURL}
           alt="Avatar"
         />
-      </dic>
+      </div>
     </div>
   );
 };
